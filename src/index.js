59ec6e26fb4f13ce16ecb727db87bc7b0560768b
happiness <happinesskhalan@gmail.com>
2025-04-20 19:44:34 +0300

    router concept complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+//import react-router functions
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
+const router = createBrowserRouter(routes);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<RouterProvider router={router} />);
